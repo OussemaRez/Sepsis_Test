1fbaaa700d29709d8001f660e6efc9622763d861
Oussema Rezguie <34216070+OussemaRez@users.noreply.github.com>
2025-10-31 00:20:38 +0000

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+# App Title
+st.title("SepsisGuard: Real-time Sepsis Prediction App")
+
+# App Description
+st.markdown("""
+Welcome to the Sepsis Prediction App!
+
+This app aims to predict the likelihood of sepsis in a patient based on various medical attributes. It includes:
+
+- Exploratory Data Analysis (EDA)
+- Predictive Data Analysis using machine learning models
+- Sepsis Prediction for individual patients
+
+**Please note:** This app is a personal project and is not intended for serious medical use. It is designed for educational and demonstration purposes only.
+
+Please explore the features to see how data is analyzed and predictions are made.
+""")
+
